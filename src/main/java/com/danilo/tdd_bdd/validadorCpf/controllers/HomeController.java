1dc0d030cb5eb21e03e5f1969333aa4be607cdd4
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-03-19 20:35:05 -0300

    template validação

--- src/main/java/com/danilo/tdd_bdd/validadorCpf/controllers/HomeController.java
+++ src/main/java/com/danilo/tdd_bdd/validadorCpf/controllers/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
   public String index(){
     return "home/index";
   }
+
+  @GetMapping("/valida-cpf")
+  public String validaCpf(){
+    return "home/cpfValidado";
+  }
 }
